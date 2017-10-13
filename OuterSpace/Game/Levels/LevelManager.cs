7f936a78feb7341dee7ed4bb48af30d5b7845b7f
Michael Chand <shadowlinux@gmail.com>
2017-10-13 16:20:24 +1300

    Implemented all the bug fixes from Rons series of emails.

--- OuterSpace/Game/Levels/LevelManager.cs
+++ OuterSpace/Game/Levels/LevelManager.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* What should this do?
+ * Pass a level to it. 
+ * The level contains information the manager uses to manage the level.
+ * The manager sets status that can be chacked on each update.
+ *      Status such as: is level still running? Is player dead. Is enemy cleared. Powerup timer/number of power ups obtained this level. Score obtained this level.
+ *      
+ * How do we determine when to transistion level?
+ */
+
 namespace OuterSpace.Game.Levels
 {
     public class LevelManager
